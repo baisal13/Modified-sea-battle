520763e581fe2eb96fbd9cc7d3dd49fba91b7460
baisal <baisal.55555@gmail.com>
2023-11-29 15:37:51 +0600

    third func

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import random
 # Function to clear the console screen
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
-<<<<<<< HEAD
 
 # Function to print the game grid
 def print_grid(grid):
@@ -12,5 +11,38 @@ def print_grid(grid):
     for i, row in enumerate(grid):
         print(f"{i + 1} {' '.join(row)}")
     print()
-=======
->>>>>>> 30e59d90048319d989a174d55a097d464804b765
+
+# Function to place ships on the grid
+def place_ships(grid):
+    ships = [[3], [2, 2], [1, 1, 1, 1]]
+
+    for ship in ships:
+        ship_placed = False
+        while not ship_placed:
+            x = random.randint(0, 6)
+            y = random.randint(0, 6)
+            orientation = random.choice(["horizontal", "vertical"])
+
+            if orientation == "horizontal":
+                if is_valid_placement(grid, ship, x, y, orientation):
+                    for i in range(len(ship)):
+                        grid[x][y + i] = str(ship[i])
+                    ship_placed = True
+            else:  # vertical
+                if is_valid_placement(grid, ship, x, y, orientation):
+                    for i in range(len(ship)):
+                        grid[x + i][y] = str(ship[i])
+                    ship_placed = True
+
+# Function to check if ship placement is valid
+def is_valid_placement(grid, ship, x, y, orientation):
+    ship_size = len(ship)
+    if orientation == "horizontal":
+        for i in range(ship_size):
+            if y + i >= 7 or grid[x][y + i] != ".":
+                return False
+    else:  # vertical
+        for i in range(ship_size):
+            if x + i >= 7 or grid[x + i][y] != ".":
+                return False
+    return True
