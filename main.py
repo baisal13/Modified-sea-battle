514b53ed165c8e5f2656caefaadcbd5f72cb274f
baisal <baisal.55555@gmail.com>
2023-12-04 15:28:27 +0600

    completely change

--- main.py
+++ main.py
@@ -1,135 +1,102 @@
 import os
 import random
 
-# Function to clear the console screen
-def clear_screen():
+# Function to create an empty 7x7 board
+def create_board():
+    return [['O' for _ in range(7)] for _ in range(7)]
+
+# Function to display the board
+def display_board(board):
     os.system('cls' if os.name == 'nt' else 'clear')
-
-# Function to print the game grid
-def print_grid(grid):
     print("   A B C D E F G")
-    for i, row in enumerate(grid):
-        print(f"{i + 1} {' '.join(row)}")
-    print()
-
-# Function to place ships on the grid
-def place_ships(grid):
-    ships = [[3], [2, 2], [1, 1, 1, 1]]
-
-    for ship in ships:
-        ship_placed = False
-        while not ship_placed:
-            x = random.randint(0, 6)
-            y = random.randint(0, 6)
-            orientation = random.choice(["horizontal", "vertical"])
-
-            if orientation == "horizontal":
-                if is_valid_placement(grid, ship, x, y, orientation):
-                    for i in range(len(ship)):
-                        grid[x][y + i] = str(ship[i])
-                    ship_placed = True
-            else:  # vertical
-                if is_valid_placement(grid, ship, x, y, orientation):
-                    for i in range(len(ship)):
-                        grid[x + i][y] = str(ship[i])
-                    ship_placed = True
+    print("  ---------------")
+    for i in range(7):
+        print(i + 1, "|", " ".join(board[i]))
+    print("\n")
 
 # Function to check if ship placement is valid
-def is_valid_placement(grid, ship, x, y, orientation):
-    ship_size = len(ship)
-    if orientation == "horizontal":
-        for i in range(ship_size):
-            if y + i >= 7 or grid[x][y + i] != ".":
-                return False
-    else:  # vertical
-        for i in range(ship_size):
-            if x + i >= 7 or grid[x + i][y] != ".":
+def is_valid(board, ship, x, y):
+    ship_x, ship_y = len(ship), 1
+    for i in range(ship_x):
+        for j in range(ship_y):
+            if x + i >= 7 or y + j >= 7 or board[x + i][y + j] != 'O':
                 return False
     return True
 
-# Function to convert coordinates (letter, number) to grid indices
-def convert_coordinates(coord):
-    col = ord(coord[0].upper()) - ord('A')
-    row = int(coord[1:]) - 1
+# Function to place ships on the board without touching each other
+def place_ships(board):
+    ships = [[3], [2, 2], [1, 1, 1, 1]]
+    for ship in ships:
+        for size in ship:
+            placed = False
+            while not placed:
+                x = random.randint(0, 6)
+                y = random.randint(0, 6)
+                if random.choice([True, False]):
+                    if is_valid(board, ship, x, y):
+                        for i in range(size):
+                            board[x + i][y] = str(size)
+                        placed = True
+                else:
+                    if is_valid(board, ship, y, x):
+                        for i in range(size):
+                            board[y + i][x] = str(size)
+                        placed = True
+    return board
+
+# Function to convert input coordinates to indices
+def convert_input(input_coord):
+    row = int(input_coord[1]) - 1
+    col = ord(input_coord[0].upper()) - ord('A')
     return row, col
 
-# Function to get the player's shot
-def get_player_shot():
-    while True:
-        try:
-            shot = input("Enter coordinates to fire (e.g., A1): ")
-            if len(shot) < 2 or not shot[0].isalpha() or not shot[1:].isdigit():
-                raise ValueError("Invalid input! Enter a letter followed by a number.")
-
-            row, col = convert_coordinates(shot)
-            if not (0 <= row < 7 and 0 <= col < 7):
-                raise ValueError("Coordinates out of bounds! Try again.")
-
-            return row, col
-        except ValueError as e:
-            print(e)
-
-# Function to update grid after a shot
-def update_grid(grid, x, y):
-    if grid[x][y] != ".":
-        ship_size = int(grid[x][y])
-        grid[x][y] = "hit"
-        # Check if the ship is sunk
-        if all(cell != str(ship_size) for row in grid for cell in row):
-            return "sunk"
-        return "hit"
+# Function to check if the shot hits a ship
+def check_shot(board, x, y):
+    if board[x][y] != 'O':
+        size = int(board[x][y])
+        board[x][y] = 'X'
+        if all('X' in row for row in board for cell in row if cell == str(size)):
+            return f"Hit! You sunk a ship of size {size}!"
+        return "Hit!"
     else:
-        grid[x][y] = "miss"
-        return "miss"
+        board[x][y] = '-'
+        return "Miss!"
 
-# Function to play the game
+# Main game function
 def play_game():
     player_name = input("Enter your name: ")
-    print(f"Hello, {player_name}! Let's play Battleship.")
-
-    shots = 0
-    game_grid = [["." for _ in range(7)] for _ in range(7)]
-    place_ships(game_grid)
-
+    print(f"Welcome, {player_name}!")
     while True:
-        clear_screen()
-        print("Your grid:")
-        print_grid(game_grid)
+        board = create_board()
+        board = place_ships(board)
+        shots = 0
+        shot_cells = set()
+        while any(str(size) in row for row in board for size in range(1, 4)):
+            display_board(board)
+            shot = input("Enter coordinates to shoot (e.g., A5): ")
+            if len(shot) != 2 or not shot[0].isalpha() or not shot[1].isdigit():
+                print("Invalid input! Please enter coordinates like 'A5'.")
+                continue
+            row, col = convert_input(shot)
+            if not (0 <= row < 7 and 0 <= col < 7):
+                print("Coordinates are out of bounds! Try again.")
+                continue
+            if (row, col) in shot_cells:
+                print("You've already shot there! Try again.")
+                continue
+            result = check_shot(board, row, col)
+            print(result)
+            shots += 1
+            shot_cells.add((row, col))
 
-        x, y = get_player_shot()
-
-        if game_grid[x][y] == "miss" or game_grid[x][y] == "hit":
-            print("You've already shot there! Try again.")
-            input("Press Enter to continue...")
-            continue
-
-        result = update_grid(game_grid, x, y)
-        shots += 1
-
-        if result == "sunk":
-            clear_screen()
-            print("Your grid:")
-            print_grid(game_grid)
-            print(f"Congratulations! You sank all ships in {shots} shots!")
+        display_board(board)
+        print(f"Congratulations, {player_name}! You won in {shots} shots!")
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
             break
 
-    return shots
+    print("Game Over!")
 
-# Function to start the game
-def start_game():
-    scores = {}
-    while True:
-        shots = play_game()
-        scores[shots] = scores.get(shots, []) + [input("Do you want to play again? (yes/no): ")]
-        if scores[shots][-1].lower() != 'yes':
-            clear_screen()
-            print("Game Over!")
-            print("Here are the scores:")
-            sorted_scores = sorted(scores.items(), key=lambda x: x[0])
-            for score, players in sorted_scores:
-                for player in players:
-                    print(f"Player: {player}, Shots: {score}")
-            break
-
+# Run the game
 if __name__ == "__main__":
-    start_game()
+    play_game()
